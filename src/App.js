a4fc789e965f49c98ae21478a47d292f5ad19280
Kevin Jensen <kevinjensen1133@yahoo.com>
2020-07-06 13:06:34 -0700

    got a lot going on

--- src/App.js
+++ src/App.js
@@ -41,11 +41,141 @@ function App() {
     });
   };
 
+  const nextSlide = () => {
+    if (testimonials.children[0].classList.contains('active')) {
+      setState({isActive1: false, isActive2: true});
+
+      fadeOut(0, 1);
+      fadeIn(1, 1);
+    } else if (testimonials.children[1].classList.contains('active')) {
+      setState({isActive2: false, isActive3: true});
+
+      fadeOut(1, 1);
+      fadeIn(2, 1);
+    } else if (testimonials.children[2].classList.contains('active')) {
+      setState({isActive3: false, isActive4: true});
+
+      fadeOut(2, 1);
+      fadeIn(0, 1);
+    }
+  }
+
+  const prevSlide = () => {
+    if (testimonials.children[0].classList.contains('active')) {
+      setState({isActive1: false, isActive3: true});
+
+      fadeOut(0, 1);
+      fadeIn(2, 1);
+    } else if (testimonials.children[1].classList.contains('active')) {
+      setState({isActive2: false, isActive1: true});
+
+      fadeOut(0, 1);
+      fadeIn(2, 1);
+    } else if (testimonials.children[2].classList.contains('active')) {
+      setState({isActive2: true, isActive3: false});
+
+      fadeOut(2, 1);
+      fadeIn(1, 1);
+    }
+  }
+
+
   return (
     <div className="App">
-      hello from app
+      <Reviews>
+        <div className = 'arrow-left' onClick = {prevSlide}>
+          <img src = {leftArrow} alt = 'left arrow' />
+        </div>
+        <div className = 't-content'>
+          <ul ref = {el => (testimonials = el)}>
+            <li className = {state.isActive1 ? 'active' : ''}>
+              <div className = 'content-inner'>
+                <p className = 'text'>{testimonials[0].text}</p>
+                <h3 className = 'name'>{testimonials[0].name}</h3>
+                <h4 className = 'location'>{testimonials[0].location}</h4>
+              </div>
+            </li>
+            <li className = {state.isActive2 ? 'active' : ''}>
+              <div className = 'content-inner'>
+                <p className = 'text'>{testimonials[1].text}</p>
+                <h3 className = 'name'>{testimonials[1].name}</h3>
+                <h4 className = 'location'>{testimonials[1].location}</h4>
+              </div>
+            </li>
+            <li className = {state.isActive3 ? 'active' : ''}>
+              <div className = 'content-inner'>
+                <p className = 'text'>{testimonials[2].text}</p>
+                <h3 className = 'name'>{testimonials[2].name}</h3>
+                <h4 className = 'location'>{testimonials[2].location}</h4>
+              </div>
+            </li>
+            <li className = {state.isActive4 ? 'active' : ''}>
+              <div className = 'content-inner'>
+                <p className = 'text'>{testimonials[3].text}</p>
+                <h3 className = 'name'>{testimonials[3].name}</h3>
+                <h4 className = 'location'>{testimonials[3].location}</h4>
+              </div>
+            </li>
+            <li className = {state.isActive5 ? 'active' : ''}>
+              <div className = 'content-inner'>
+                <p className = 'text'>{testimonials[4].text}</p>
+                <h3 className = 'name'>{testimonials[4].name}</h3>
+                <h4 className = 'location'>{testimonials[4].location}</h4>
+              </div>
+            </li>
+            <li className = {state.isActive6 ? 'active' : ''}>
+              <div className = 'content-inner'>
+                <p className = 'text'>{testimonials[5].text}</p>
+                <h3 className = 'name'>{testimonials[5].name}</h3>
+                <h4 className = 'location' >{testimonials[5].location}</h4>
+              </div>
+            </li>
+            <li className = {state.isActive7 ? 'active' : ''}>
+              <div className = 'content-inner'>
+                <p className = 'text'>{testimonials[6].text}</p>
+                <h3 className = 'name'>{testimonials[6].name}</h3>
+                <h4 className = 'location'>{testimonials[6].location}</h4>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div className = 'arrow-right' onClick = {nextSlide}>
+          <img src = {rightArrow} alt = 'right arrow' />
+        </div>
+      </Reviews>
     </div>
   );
 }
 
 export default App;
+
+const Reviews = styled.div `
+  
+  .arrow-left {
+
+  }
+  .arrow-right {
+
+  }
+  .t-content {
+
+    ul {
+
+      li {
+
+        .content-inner {
+
+          .text {
+
+          }
+          .name {
+
+          }
+          .location {
+
+          }
+        }
+      }
+    }
+  }
+`
